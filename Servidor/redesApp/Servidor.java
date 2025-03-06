daa6165b863b0254f3b9f000f060ded1598d6707
Jessica de K. <jessicauchiha01@gmail.com>
2025-03-05 21:49:55 -0300

    Add files via upload

--- Servidor/redesApp/Servidor.java
+++ Servidor/redesApp/Servidor.java
@@ -1,5 +1,7 @@
 package redesApp;
 
+import redesApp.service.ServidorService;
+
 /**
  *
  * @author jessi
@@ -11,6 +13,7 @@ public class Servidor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        new ServidorService();
     }
     
 }
