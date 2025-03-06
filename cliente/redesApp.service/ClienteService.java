9d36d60229dde7b51372ae04ed58fff663c5f7bc
Jessica de K. <jessicauchiha01@gmail.com>
2025-03-05 21:45:35 -0300

    Add files via upload

--- cliente/redesApp.service/ClienteService.java
+++ cliente/redesApp.service/ClienteService.java
@@ -1,1 +1,42 @@
+package redesApp.service;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import redesApp.bean.ChatMessage;
+
+/**
+ *
+ * @author jessi
+ */
+public class ClienteService {
+    
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect(){
+        try {
+            this.socket = new Socket("localhost", 5555);     //Socket(String, host, int port), passando o host e o servidor socket está. Pode ser o IP ou nome da máquina e a porta de conexão
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return socket;
+    }
+    
+    
+    //Método send --> Para enviar as mensagens
+    public void send(ChatMessage message){
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    
+}
